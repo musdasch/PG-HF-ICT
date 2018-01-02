571809b3000f5de7207cffed2e65970cdb775c9a
Tim <musdasch@gmail.com>
2018-01-02 11:40:04 +0100

    add task 6.1

---
+++ task_06_1.cpp
@@ -0,0 +1,36 @@
+/**
+ * The task 6.1 is write a program which takes
+ * two masses and calculates the gravitation.
+ *
+ * @author Tim Voegtli
+ * @version 1.0
+ */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	double m1(0), m2(0), r(0), Fg(0);
+	const double f(0.00000000006673);
+
+	cout << "Gravitation Berechnen:" << endl;
+	
+	cout << "Masse 1: ";
+	cin >> m1;
+
+	cout << "Masse 2: ";
+	cin >> m2;
+
+	cout << "Abstand: ";
+	cin >> r;
+
+	Fg = ((m1 * m2)/pow(r,2))*f;
+
+	cout << "Gravitation: ";
+	cout << Fg;
+
+
+	return 0;
+}
