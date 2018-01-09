9fc7f488bcd311867e3942243ed725fa0348f5c6
Tim <musdasch@gmail.com>
2018-01-09 15:37:15 +0100

    add task 12.3.1

--- task_12_3_1.cpp
+++ task_12_3_1.cpp
@@ -7,26 +7,73 @@
  * @version 1.0
  */
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+/**
+ * Finds the complement for a given range.
+ * @param  value negative number
+ * @param  range range in which the number operates.
+ * @return       complement
+ */
+int complement(int value, int range){
+	value = sqrt(pow(value, 2));
+	value = range - value;
+
+	return value;
+}
+
+/**
+ * encrypt or decrypt a character depending if the key is a complement or not 
+ * @param  character Character to crypt.
+ * @param  key       Key with which to crypt.
+ * @param  start     Start of number space.
+ * @param  range     Range of number space.
+ * @return           returns Crypt-ed character.
+ */
+char cryptCharacter(char character, int key, int start, int range){
+	int tmp(character);
+
+	if( tmp >= start && tmp < start + range){
+		tmp = (int) tmp + key - start;
+		tmp = tmp % range;
+		tmp += start;
+
+		cout << tmp << endl;
+	}
+
+	return (char) tmp;
+}
+
+/**
+ * Function to encrypt.
+ * @param array Array of character.
+ * @param size  Size of the array.
+ * @param key   Key with which to encrypt.
+ */
 void encrypt( char *array, int size, int key ){
-	char k(0);
-
-	k = (char) key % sizeof(char);
-	cout << k;
 	for(int i(0); i < size; i++){
-		//cout << (int) array[i];
-		//array[i] = (char)((int) array[i] + key) % sizeof(char);
+		array[i] = cryptCharacter(array[i], key, 65, 26);
+		array[i] = cryptCharacter(array[i], key, 97, 26);
 	}
 }
 
+/**
+ * Function to decrypt.
+ * @param array Array of character.
+ * @param size  Size of the array.
+ * @param key   Key with which to decrypt.
+ */
 void decrypt( char *array, int size, int key ){
-	for(int i(0); i < size; i++){
-		array[i] = (array[i] - key) % sizeof(char);
-	}
+	key = complement(key, 26);
+	encrypt(array, size, key);
 }
 
+/**
+ * Main function.
+ * @return Hopefully zero.
+ */
 int main(){
 	int key(0);
 	char input[100]; 
@@ -38,11 +85,22 @@ int main(){
 	cout << "Schluessel eingeben: ";
 	cin >> key;
 
+	if(0 > key){
+		key = complement(key, 26);
+	}
+
 	cout << "Wort eingeben: ";
 	cin >> input;
 
+	cout << "Der Schlüssel lautet: " << key << endl;
+
 	encrypt(input, 100, key);
-	cout << input;
+
+	cout << "Der verschlüsselte Text lautet: " << input << endl;
+
+	decrypt(input, 100, key);
+
+	cout << "Der entschlüsselte Text lautet: " << input << endl;
 
 	return 0;
 }
